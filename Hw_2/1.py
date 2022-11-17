71f7564c76f464394dc29beb345f2bb20f940d82
Dpvit <dpvitushkin1@yandex.ru>
2022-11-17 19:21:41 +0300

    17.11.2022

---
+++ Hw_2/1.py
@@ -0,0 +1,23 @@
+"""
+    Витушкин Денис
+    Составьте программу, которая запрашивает название футбольной команды и повторяет его на экране со словами
+"""
+
+name = str(input("Enter name: "))
+print(name,"- чемпион!")
+
+print("-"*len(name))
+
+name=name.lower()
+print("len = ", len(name))
+
+if 'п' in name:
+    print(True)
+else:
+    print(False)
+
+counter = 0
+for char in name:
+    if char == 'а':
+        counter+=1
+print(counter)
